02aca10b05ab6cc3cf820af6ad417e91b6ceb77f
藤井 勇気 <c2388@navi.cbo.mss.co.jp>
2018-03-28 18:04:07 +0900

    gtags update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -418,17 +418,7 @@ scroll-step 20)
 (add-hook 'c-mode-hook 'gtags-mode)
 (add-hook 'c++-mode-hook 'gtags-mode)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; rtags.el
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(when (require 'rtags nil 'noerror)
-  (add-hook 'c-mode-common-hook
-            (lambda ()
-              (when (rtags-is-indexed)
-                (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
-                (local-set-key (kbd "M-;") 'rtags-find-symbol)
-                (local-set-key (kbd "M-@") 'rtags-find-references)
-                (local-set-key (kbd "M-,") 'rtags-location-stack-back)))))
+(setq gtags-path-style 'relative)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; auto-async-byte-compile.el
